47ffc64f6ba1d686a632c0467b1d954286be6f2d
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-10-20 15:46:39 +0200

    ➕ feat: bouton \"Nouveau\" et workflow historique des parts

---
+++ front/partshistory.php
@@ -0,0 +1,28 @@
+<?php
+
+require '../../../inc/includes.php';
+
+Session::checkLoginUser();
+Session::checkRight('plugin_associatesmanager', READ);
+
+Html::header(
+   PluginAssociatesmanagerPartshistory::getTypeName(Session::getPluralNumber()),
+   $_SERVER['PHP_SELF'],
+   'admin',
+   'PluginAssociatesmanagerMenu',
+   'partshistory'
+);
+
+// Add "New" button in header if user has CREATE right
+if (Session::haveRight('plugin_associatesmanager', CREATE)) {
+   echo "<div class='spaced'>";
+   echo "<a href='" . PluginAssociatesmanagerPartshistory::getFormURL() . "' class='btn btn-primary'>";
+   echo "<i class='fas fa-plus'></i> ";
+   echo "<span>" . __('New parts history', 'associatesmanager') . "</span>";
+   echo "</a>";
+   echo "</div>";
+}
+
+Search::show('PluginAssociatesmanagerPartshistory');
+
+Html::footer();
