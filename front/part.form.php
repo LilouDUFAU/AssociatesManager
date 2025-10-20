3267027aa69f238ddfeed7e594cbf98c633cf008
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-10-20 15:49:26 +0200

    📝 feat: amélioration du formulaire part (validation, droits, redirections)

---
+++ front/part.form.php
@@ -0,0 +1,45 @@
+<?php
+
+require '../../../inc/includes.php';
+
+Session::checkLoginUser();
+Session::checkRight('plugin_associatesmanager', READ);
+
+$part = new PluginAssociatesmanagerPart();
+
+if (isset($_POST['add'])) {
+   // Always set entities_id for new parts
+   if (!isset($_POST['entities_id']) || !$_POST['entities_id']) {
+      $_POST['entities_id'] = $_SESSION['glpiactive_entity'];
+   }
+   $part->check(-1, CREATE, $_POST);
+   if ($newID = $part->add($_POST)) {
+      // Always redirect to the list after creation
+      Html::redirect(Plugin::getWebDir('associatesmanager') . '/front/part.php');
+   }
+   Html::back();
+
+} else if (isset($_POST['update'])) {
+   $part->check($_POST['id'], UPDATE);
+   $part->update($_POST);
+   // Redirect to the part's detail page after update
+   Html::redirect($part->getLinkURL());
+
+} else if (isset($_POST['delete'])) {
+   $part->check($_POST['id'], DELETE);
+   $part->delete($_POST);
+   // Always redirect to the list after deletion
+   Html::redirect(Plugin::getWebDir('associatesmanager') . '/front/part.php');
+
+} else if (isset($_POST['purge'])) {
+   $part->check($_POST['id'], PURGE);
+   $part->delete($_POST, 1);
+   // Always redirect to the list after purge
+   Html::redirect(Plugin::getWebDir('associatesmanager') . '/front/part.php');
+
+} else {
+   $part->checkGlobal(READ);
+
+   $menus = ['admin', 'PluginAssociatesmanagerMenu', 'part'];
+   PluginAssociatesmanagerPart::displayFullPageForItem($_GET['id'] ?? 0, $menus, $_GET);
+}
