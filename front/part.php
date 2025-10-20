b5b4ddfe67668c0f8109d7c3e11ab9971387b16b
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-10-20 15:46:18 +0200

    ➕ feat: bouton \"Nouveau\" et workflow parts

---
+++ front/part.php
@@ -0,0 +1,28 @@
+<?php
+
+require '../../../inc/includes.php';
+
+Session::checkLoginUser();
+Session::checkRight('plugin_associatesmanager', READ);
+
+Html::header(
+   PluginAssociatesmanagerPart::getTypeName(Session::getPluralNumber()),
+   $_SERVER['PHP_SELF'],
+   'admin',
+   'PluginAssociatesmanagerMenu',
+   'part'
+);
+
+// Add "New" button in header if user has CREATE right
+if (Session::haveRight('plugin_associatesmanager', CREATE)) {
+   echo "<div class='spaced'>";
+   echo "<a href='" . PluginAssociatesmanagerPart::getFormURL() . "' class='btn btn-primary'>";
+   echo "<i class='fas fa-plus'></i> ";
+   echo "<span>" . __('New part', 'associatesmanager') . "</span>";
+   echo "</a>";
+   echo "</div>";
+}
+
+Search::show('PluginAssociatesmanagerPart');
+
+Html::footer();
