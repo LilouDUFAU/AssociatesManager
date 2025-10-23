b7ef60ac3638cfce50eeb2bbf1155e749212a270
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-10-23 11:19:14 +0200

    🧹 chore(sql): ajouter script SQL de désinstallation (DROP TABLE)

---
+++ install/mysql/plugin_associatesmanager_uninstall.sql
@@ -0,0 +1,5 @@
+-- Uninstall SQL for AssociatesManager plugin
+DROP TABLE IF EXISTS `glpi_plugin_associatesmanager_configs`;
+DROP TABLE IF EXISTS `glpi_plugin_associatesmanager_partshistories`;
+DROP TABLE IF EXISTS `glpi_plugin_associatesmanager_parts`;
+DROP TABLE IF EXISTS `glpi_plugin_associatesmanager_associates`;
